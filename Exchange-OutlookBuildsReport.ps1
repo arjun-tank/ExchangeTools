c108224fa10a0ca8a1329211fa8cd328931b2ad5
Arjun Tank <78632246+arjun-tank@users.noreply.github.com>
2021-03-22 16:16:03 -0700

    Update Exchange-OutlookBuildsReport.ps1
    Changing report to extract data from last 4 days instead of last 2 days. Adding more comments.

--- Exchange-OutlookBuildsReport.ps1
+++ Exchange-OutlookBuildsReport.ps1
@@ -7,10 +7,19 @@
 # Exchange 2013 and later Path:
 $logpath = 'C:\Program Files\Microsoft\Exchange Server\V15\Logging\RPC Client Access'
 
-$files = Get-ChildItem $logpath -ea 1 |Where-Object {$_.LastWriteTime -ge (Get-Date).AddDays(-2)}
+# Get log files for the last 4 days
+$files = Get-ChildItem $logpath -ea 1 |Where-Object {$_.LastWriteTime -ge (Get-Date).AddDays(-4)}
+
+# Remove headers and import in to variable
 $logs = $files | ForEach {Get-Content $_.FullName}| Where-Object {$_ -notlike '#*'}
 $result = $logs |ConvertFrom-Csv -Header date-time,session-id,seq-number,client-name,organization-info,client-software,client-software-version,client-mode,client-ip,server-ip,protocol,application-id,operation,rpc-status,processing-time,operation-specific,failures
+
+# Search for clients that show outlook.exe under client-software
 $uniqueClients = $result | Where-Object {$_.'client-software' -eq 'OUTLOOK.EXE'}| select client-name,client-ip,client-software-version,client-software | sort client-name -unique
+
+# Prepare a report with recipient information and Outlook version number
 $fr = @()
 $uniqueClients | %{ $rt = get-recipient $_."client-name" ; $obj=""|select Email,Build,RecType ; $obj.Email=$rt.PrimarySMTPAddress;$obj.Build=$_."client-software-version";$obj.RecType=$rt.RecipientTypeDetails; $fr += $obj;}
+
+# Export report to a CSV file
 $fr | export-csv Outlook_BuildsEmail.csv -notype
